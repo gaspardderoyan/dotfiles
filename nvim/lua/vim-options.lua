bb6a44d05029c676170ea48af15ed866c209b4b1
Gaspard Deroyan <gaspardderoyan@gaspards-mbp.home>
2024-07-03 01:55:40 +0100

    Added nvim config

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,10 @@
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.g.mapleader = ' '
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.number = true
+vim.opt.relativenumber = true
